546dcb7f316372048910f6e56df7e452153164af
mateeusferro <mateus.03fernandesaraujo@gmail.com>
2024-08-19 16:28:49 -0300

    feat: adding new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { BuyListModule } from './buy-list/buy-list.module';
 import { ItemsModule } from './items/items.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -12,6 +14,7 @@ import { ItemsModule } from './items/items.module';
     }),
     BuyListModule,
     ItemsModule,
+    PrismaModule,
   ],
   controllers: [],
   providers: [],

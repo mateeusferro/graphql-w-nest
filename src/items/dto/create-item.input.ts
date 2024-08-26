331b15bb2e441ab7b683525df84822db7cc8063c
mateeusferro <mateus.03fernandesaraujo@gmail.com>
2024-08-26 13:00:26 -0300

    fix: fixing an error on dto to create an item

--- src/items/dto/create-item.input.ts
+++ src/items/dto/create-item.input.ts
@@ -1,8 +1,20 @@
-import { Field, InputType, OmitType } from "@nestjs/graphql"
+import { Field, InputType, Int, OmitType } from "@nestjs/graphql"
 import { Item } from "../entities/item.entity"
 
 @InputType()
 export class CreateItemInput extends OmitType(Item, ['id', 'createdAt', 'updatedAt']) {
-    @Field()
-    name2: String
+    @Field(() => String)
+    name: string
+
+    @Field(() => Int)
+    quantity: number
+
+    @Field(() => String)
+    category: string
+
+    @Field(() => Boolean)
+    status: boolean
+
+    @Field(() => Int)
+    buyListId: number
 }
